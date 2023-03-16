d0e6a0f1f47e071cf568d3cb3b3e5f128c3ba840
Loïc <neatstudiobe@gmail.com>
2023-03-16 11:30:36 +0000

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <main>
+    <!-- https://image.nuxtjs.org/components/nuxt-picture -->
+    <!-- https://image.nuxtjs.org/components/nuxt-img -->
+    <figure>
+      <nuxt-img
+        format="webp"
+        src="https://www.eib.org/photos/download.do?documentId=fa3e036a-d75a-433e-8acf-8138bed86700&binaryType=XL"
+        alt=""
+      />
+
+      <figcaption class="text-center text-gray-600 text-xs mt-3">
+        Photo by someone
+      </figcaption>
+    </figure>
+  </main>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped></style>
